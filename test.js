576d00bf4afea762230b8a1c19f822d8cf2792c7
nsavinov <aldouswhite@gmail.com>
2024-04-22 19:21:40 +0300

    []

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 console.log(process.env.DATABASE_URL);
-console.log("First 5 characters of the secret: " + process.env.DATABASE_URL.substring(0, 60));
+console.log("First 5 characters of the secret: " + process.env.DATABASE_URL.substring(0, 20));
+console.log("First 5 characters of the secret: " + process.env.DATABASE_URL.substring(0, 30));
+console.log("First 5 characters of the secret: " + process.env.DATABASE_URL.substring(30, 40));
+console.log("First 5 characters of the secret: " + process.env.DATABASE_URL.substring(40, 50));
+console.log("First 5 characters of the secret: " + process.env.DATABASE_URL.substring(process.env.DATABASE_URL.length-76, process.env.DATABASE_URL.length));
 console.log("Length of the secret: " + process.env.DATABASE_URL.length);
 console.log(process.env.DATABASE_URL === "postgresql://neondb_owner:uokncgyia0P5@ep-small-bonus-w049d3yh.cloud.hydrogen.aws.neon.build/neondb?sslmode=require")
 console.log(process.env.DATABASE_URL === "postgresql://neondb_owner:uokncgyia0P5@ep-small-bonus-w049d3yh-pooler.cloud.hydrogen.aws.neon.build/neondb?sslmode=require")
