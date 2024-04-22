2c971a86bb01f4cf5dfe80600fde3da40e9dcb6b
nsavinov <aldouswhite@gmail.com>
2024-04-22 19:10:35 +0300

    []

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
-import assert from "node:assert";
-import test from "node:test";
-import { addUser, createUserTable, getUserCount } from "./lib/database.js";
 
-test("that INSERT added 1 row", async () => {
-  await createUserTable();
-  const before = await getUserCount();
-  await addUser();
-  const after = await getUserCount();
-  assert.strictEqual(before + 1, after);
-});
+console.log(process.env.DATABASE_URL);
+
+// import assert from "node:assert";
+// import test from "node:test";
+// import { addUser, createUserTable, getUserCount } from "./lib/database.js";
+//
+// test("that INSERT added 1 row", async () => {
+//   await createUserTable();
+//   const before = await getUserCount();
+//   await addUser();
+//   const after = await getUserCount();
+//   assert.strictEqual(before + 1, after);
+// });
