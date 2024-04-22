22300601d32c88097051d3fea85f5f609679e25a
nsavinov <aldouswhite@gmail.com>
2024-04-22 19:12:27 +0300

    []

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-
 console.log(process.env.DATABASE_URL);
+console.log("First 5 characters of the secret: " + process.env.DATABASE_URL.substring(0, 10));
+console.log("Length of the secret: " + process.env.DATABASE_URL.length);
 
 // import assert from "node:assert";
 // import test from "node:test";
