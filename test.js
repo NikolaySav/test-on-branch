6863a47668621ab0eb3e4fd853c5a7eb6b320d91
nsavinov <aldouswhite@gmail.com>
2024-04-23 11:33:50 +0300

    []

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 import assert from "node:assert";
 import test from "node:test";
-import {addUser, createUserTable, getUserCount} from "./lib/database.js";
+import { addUser, createUserTable, getUserCount } from "./lib/database.js";
 
 test("that INSERT added 1 row", async () => {
-    await createUserTable();
-    const before = await getUserCount();
-    await addUser();
-    const after = await getUserCount();
-    assert.strictEqual(before + 1, after);
+  await createUserTable();
+  const before = await getUserCount();
+  await addUser();
+  const after = await getUserCount();
+  assert.strictEqual(before + 1, after);
 });
